ff32a2e2f0df0d3d91cbea90cb9d0b47c9912086
FilloyLuca <filloylucapro@gmail.com>
2025-12-05 16:35:20 +0100

    Ajout du dossier componenent

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Header } from './component/header/header';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header
   ],
   imports: [
     BrowserModule,
